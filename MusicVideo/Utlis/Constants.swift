a856cde69d43ccdb0c83dedc23bfcc2873d6c1cf
Sreeram R <sreeram888@gmail.com>
2016-08-09 20:16:29 +0530

    Part 7 Completed

--- MusicVideo/Utlis/Constants.swift
+++ MusicVideo/Utlis/Constants.swift
@@ -11,3 +11,9 @@ import Foundation
 typealias JSONDictonary = [String:AnyObject]
 
 typealias JSONArray =   Array<AnyObject>
+
+let WIFI = "WIFI Avaliable"
+
+let NOACCESS = "No Internet Access"
+
+let WWAN = "Cellular Data"
